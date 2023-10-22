d1a80f876ed3cf76f1dcb7fc41ca938f860f8b99
dipteshrastogi <dipteshraj2003@gmail.com>
2023-10-22 11:12:16 +0300

    receiver.js not working

--- geolocation.js
+++ geolocation.js
@@ -3,33 +3,18 @@ function getLocationWithGeolocationAPI() {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
-        // Send latitude and longitude data to the receiver endpoint
-        sendDataToReceiver(latitude, longitude);
-
         // Update UI with location info
         const locationInfo = `Location (Geolocation API): Latitude: ${latitude}, Longitude: ${longitude}`;
         document.getElementById("location-info").textContent = locationInfo;
-    }, function(error) {
-        // If Geolocation API fails, fall back to IP address-based location retrieval
-        getLocationWithIPAddress();
-    });
-}
 
-function sendDataToReceiver(latitude, longitude) {
-    fetch("http://localhost:3000/receive-location", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ latitude, longitude })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Receiver response:", data);
-        // Assuming the response contains any necessary acknowledgments or data
-    })
-    .catch(error => {
-        console.error("Error sending data to receiver:", error);
+        // Send latitude and longitude data to the server
+        fetch('/send-location', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ latitude, longitude }),
+        });
     });
 }
 
--- receiver.js
+++ receiver.js
@@ -1,37 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
+const port = 3000;
+
+// Parse JSON bodies
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
 
-app.post('/receive-location', (req, res) => {
+// Serve your HTML file and client-side JavaScript
+app.use(express.static('public'));
+
+// Add a GET route handler for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Endpoint to receive latitude and longitude data
+app.post('/send-location', (req, res) => {
     const { latitude, longitude } = req.body;
-    console.log('Received location data - Latitude:', latitude, 'Longitude:', longitude);
-    // Process the received latitude and longitude data here
-
-    // Assuming you want to send the same data to server.js
-    sendDataToServer(latitude, longitude);
-
-    // Send a response to acknowledge the receipt of location data
-    res.status(200).json({ message: 'Location data received and forwarded successfully' });
+    console.log(`Received latitude: ${latitude}, longitude: ${longitude}`);
+    res.send('Location data received successfully!');
 });
 
-const serverPort = 3001; // Change the port to avoid conflicts with the frontend
-app.listen(serverPort, () => {
-    console.log(`Receiver listening on port ${serverPort}`);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-function sendDataToServer(latitude, longitude) {
-    const serverURL = 'http://server-system-ip:3000/process-location'; // Replace with the server's IP address
-    const data = { latitude, longitude };
-    axios.post(serverURL, data)
-        .then(response => {
-            console.log('Data sent to server successfully');
-            console.log('Response from server:', response.data);
-        })
-        .catch(error => {
-            console.error('Error sending data to server:', error);
-        });
-}
